9fe6a9a0c5cfe4e798abb5cbefcc27a8e546c5d6
richard <529565267@qq.com>
2018-11-28 21:11:36 +0800

    juc

--- Concurrency/src/main/java/com/example/atomic/AtomicBolean.java
+++ Concurrency/src/main/java/com/example/atomic/AtomicBolean.java
@@ -1,11 +1,24 @@
 package com.example.atomic;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class AtomicBolean {
 
+	//AtomicBoolean的 compareAndSet 方法 常用来保证代码只执行一次
+	private static AtomicBoolean flag = new AtomicBoolean(false);
+	
 	public static void main(String[] args) {
 		
+		for(int i=0; i<10; i++) {
+			test();
+		}
 		
 		
-		
+	}
+	
+	public static void test() {
+		if (flag.compareAndSet(false, true)) {
+			System.out.println("执行");
+		}
 	}
 }
--- Concurrency/src/main/java/com/example/demo/ConcurrencyApplication.java
+++ Concurrency/src/main/java/com/example/demo/ConcurrencyApplication.java
@@ -2,6 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.example.threadlocal.HttpFilter;
+
 
 @SpringBootApplication
 public class ConcurrencyApplication {
@@ -9,4 +14,13 @@ public class ConcurrencyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConcurrencyApplication.class, args);
 	}
+	//注册过滤器
+	@Bean
+	public FilterRegistrationBean httpFilter() {
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+		filterRegistrationBean.setFilter(new HttpFilter());
+		filterRegistrationBean.addUrlPatterns("/*");
+		return filterRegistrationBean;
+	}
+	
 }
